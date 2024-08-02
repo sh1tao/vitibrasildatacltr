800b50ecc7e46880721b5bef7e6dfd606f81de80
sh1tao <wssantos1993@gmail.com>
2024-08-02 18:33:46 -0300

    +docker deploy +docker-compose +Habilitando conexao local

--- run.py
+++ run.py
@@ -26,4 +26,4 @@ app = create_app()
 if __name__ == "__main__":
     show_usage()
     print("Inicializando o aplicativo...")  # Mensagem antes de iniciar o servidor
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=5000)
